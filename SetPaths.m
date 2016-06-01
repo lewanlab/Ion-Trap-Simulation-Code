3b205459deadb60189092dced36b60e55d5b904d
unknown <bentine@ALWSDAQ71.physics.ox.ac.uk>
2016-06-01 19:45:16 +0100

    Moved plotting functions to their own folder

--- SetPaths.m
+++ SetPaths.m
@@ -4,4 +4,5 @@ addpath(genpath(sprintf('%s\\Analysis', cd)))
 addpath(genpath(sprintf('%s\\DCDplotting', cd)))
 addpath(genpath(sprintf('%s\\Tools', cd)))
 addpath(genpath(sprintf('%s\\Util', cd)))
+addpath(genpath(sprintf('%s\\Plot', cd)))
 end
