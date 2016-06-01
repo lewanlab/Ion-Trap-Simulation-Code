9f54e078fbf3d29324528eda32ca3b47d367a46c
unknown <bentine@ALWSDAQ71.physics.ox.ac.uk>
2016-06-01 20:19:54 +0100

    renamed and removed one minimisation script example

--- LAMMPS wrapper/Util/getVoltageForLinearTrapAQ.m
+++
@@ -1,8 +0,0 @@
-function [ OscV, Endcap ] = getVoltageForLinearTrapAQ( ionCharge, ionMass, RFFrequency, z0, r0, geometricC, trapAx, trapQx )
-%GETVOLTAGEFORLINEARTRAPAQ Calculates the voltage required for given a,q
-%operation of the specified trap.
-
-Endcap = trapAx * ionMass * z0^2 * (2*pi*RFFrequency)^2/-(geometricC*4*ionCharge);
-OscV = trapQx * -(ionMass * r0^2 * (2*pi*RFFrequency)^2) / (2*ionCharge);
-
-end
