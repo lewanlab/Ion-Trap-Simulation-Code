d2cb1309434f381f4670f13918e1723e6e2e3c71
unknown <bentine@ALWSDAQ71.physics.ox.ac.uk>
2016-06-29 00:18:28 +0100

    Adding in InputFileElement instead of all that cfg mess. A few things are broken as a result - next up will be to fix variables and add a better way of handling atom groups (get rid of the now broken TypeGroupPrefix

--- LAMMPS wrapper/cfgObjects/cfgObject.m
+++
@@ -1,7 +0,0 @@
-classdef cfgObject < matlab.mixin.Heterogeneous & handle
-    %CFGOBJECT Objects that implement LAMMPS commands in the input file.    
-    properties
-        cfgFileHandle % A function that generates text for the lammps input file
-    end   
-end
-
--- Verification/PhaseTransitions/CalculateA.m
+++
@@ -1,9 +0,0 @@
-function [ a ] = CalculateA( anisotropy, q )
-%CALCULATEA Calculates the required a to give desired anisotropy parameter
-%at known q.
-% This assumes that q_r >> a_r.
-a = (anisotropy .* (q .^ 2)/4);
-a = -abs(a);
-
-end
-
