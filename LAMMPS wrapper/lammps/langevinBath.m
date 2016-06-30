70bb1b6cf98d14ab4e8cc0c0cce3e058f9260368
unknown <bentine@ALWSDAQ71.physics.ox.ac.uk>
2016-06-30 09:36:08 +0100

    Langevin bath now uses new group syntax

--- LAMMPS wrapper/lammps/langevinBath.m
+++ LAMMPS wrapper/lammps/langevinBath.m
@@ -1,4 +1,4 @@
-function [ fix ] = langevinBath( temperature, dampingTime, ionSpecies )
+function [ fix ] = langevinBath( temperature, dampingTime, group )
 %LANGEVINBATH Creates a langevin bath of a given temperature (in Kelvin).
 % The langevin bath applies a viscous damping force to each atom
 % proportional to its velocity plus a stochastic, white noise force. The
@@ -7,13 +7,13 @@ function [ fix ] = langevinBath( temperature, dampingTime, ionSpecies )
 % damping time (s) is the time taken for velocity to relax to 1/e its initial
 % value in a zero temperature bath.
 %
-% ionSpecies is an optional parameter that allows the Langevin bath to be
-% coupled to one ion species only. If left unspecified the bath is coupled
-% to all atoms.
+% group is an optional parameter that allows the Langevin bath to be
+% coupled to only some atoms. If left unspecified the bath is coupled to
+% all atoms.
 %
-% Syntax: langevinBath( temperature, dampingTime, ionSpecies )
+% Syntax: langevinBath( temperature, dampingTime, group )
 %
-% Example: sim.Add( langevinBath(4, 1e-4, calcium) )
+% Example: sim.Add( langevinBath(4, 1e-4, sim.Group(calciumIons)) )
 %
 % See Also: laserCool, http://lammps.sandia.gov/doc/fix_langevin.html
 
@@ -23,7 +23,8 @@ if nargin < 3
     fix.createInputFileText = @langevinBathCfg;
     fix.InputFileArgs = { temperature, dampingTime };
 else
-    fix.createInputFileText = @()typeGroupPrefix(@(g)langevinBathCfg(fix.ID, temperature, dampingTime, g), ionSpecies.id);
+    fix.createInputFileText = @langevinBathCfg;
+    fix.InputFileArgs = { temperature, dampingTime, group };
 end
 
 
--- LAMMPS wrapper/lammps/private/langevinBathCfg.m
+++ LAMMPS wrapper/lammps/private/langevinBathCfg.m
@@ -1,12 +1,18 @@
-function [ strings ] = langevinBathCfg( fixID, temperature, dampingTime, groupID )
+function [ strings ] = langevinBathCfg( fixID, temperature, dampingTime, group )
 %LANGEVINBATHCFG Creates the cfg file txt for a langevin bath
 
 if nargin < 4
-    groupID = 'all';
+    groupName = 'all';
+else
+    if ~isa(group, 'LAMMPSGroup')
+       error('group variable must be of type LAMMPSGroup'); 
+    end
+    
+    groupName = group.ID;
 end
 
 strings = { '#Adding a langevin bath...' };
-strings{end+1} = sprintf('fix %s %s langevin %e %e %e 1337', fixID, groupID, temperature, temperature, dampingTime); 
+strings{end+1} = sprintf('fix %s %s langevin %e %e %e 1337', fixID, groupName, temperature, temperature, dampingTime); 
 strings{end+1} =  '';
 
 end
