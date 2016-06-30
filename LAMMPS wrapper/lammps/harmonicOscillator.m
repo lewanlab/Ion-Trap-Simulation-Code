442e96fbf924640efb2039833d4da530eacd8477
unknown <bentine@ALWSDAQ71.physics.ox.ac.uk>
2016-06-30 09:31:39 +0100

    Harmonic oscillator now specified in frequencies (makes more sense) and uses new group syntax

--- LAMMPS wrapper/lammps/harmonicOscillator.m
+++ LAMMPS wrapper/lammps/harmonicOscillator.m
@@ -1,9 +1,15 @@
-function [ fix ] = harmonicOscillator( k_x, k_y, k_z, atomType )
-%HARMONICOSCILLATOR Adds a simple harmonic oscillator to the given species (all atoms if no species
-%specified).
-% k_x, k_y, k_z are spring constants along the three cartesian axes.
-%
-% See Also: CylindricalSHO, linearSecularPaulTrap
+function [ fix ] = harmonicOscillator( f_x, f_y, f_z, group )
+%HARMONICOSCILLATOR Adds a simple harmonic oscillator to the given group of
+%atoms (all atoms if no group specified).
+% f_x, f_y, f_z are spring constants along the three cartesian axes.
+% SYNTAX: harmonicOscillator( f_x, f_y, f_z )
+%         harmonicOscillator( f_x, f_y, f_z, group )
+
+if nargin > 3
+    if ~isa(group, 'LAMMPSGroup')
+        error('group argument must be of type LAMMPSGroup');
+    end
+end
 
 if k_x < 0 || k_y < 0 || k_z < 0
     warning('One or more spring constants are less than zero (repulsive)');
@@ -11,16 +17,14 @@ end
 
 fix = LAMMPSFix();
 
-if nargin > 2
+if nargin > 3
     %calculate frequencies for the atomtype to determine limit on timestep.
-    au = 1.6605e-27;
-    mass = atomType.mass * au;
-    f = sqrt(max(k_y, max(k_x, k_z)) / mass) /(2*pi);
-    fix.time = 1/(10*f);
-    fix.cfgFileHandle = @()typeGroupPrefix(@(g)SHOCfg(fix.ID, k_x, k_y, k_z, g), atomType.id);
+    fix.time = 1/(10*max([f_x, f_y, f_z]));
+    fix.createInputFileText = @harmonicOscillatorCfg;
+    fix.InputFileArgs = { f_x, f_y, f_z, group };
 else
-    fix.createInputFileText = @SHOCfg;
-    fix.InputFileArgs = { k_x, k_y, k_z };
+    fix.createInputFileText = @harmonicOscillatorCfg;
+    fix.InputFileArgs = { f_x, f_y, f_z };
 end
 
 
---
+++ LAMMPS wrapper/lammps/private/harmonicOscillatorCfg.m
@@ -0,0 +1,29 @@
+function [ strings ] = harmonicOscillatorCfg(fixID, f_x, f_y, f_z, group )
+%SHOCFG Creates a harmonic oscillator centred around the origin.
+% If a group is specified the fix will apply to only those atoms.
+
+if (nargin < 4)
+    groupName = 'all'; %Fix for all atoms if no atomType is specified.
+else
+    if ~isa(group, 'LAMMPSGroup')
+       error('group must be a LAMMPSGroup'); 
+    end
+    
+    groupName = group.ID;
+end
+
+strings =           {'# Harmonic Oscillator: '};
+strings{end+1} =     sprintf('variable %s_w_x\t\tequal %e', fixID, 2*pi*f_x);
+strings{end+1} =     sprintf('variable %s_w_y\t\tequal %e', fixID, 2*pi*f_y);
+strings{end+1} =     sprintf('variable %s_w_z\t\tequal %e', fixID, 2*pi*f_z);
+strings{end+1} =     sprintf('variable fX%s atom "-v_%s_k_x * x * mass"', fixID, fixID);
+strings{end+1} =     sprintf('variable fY%s atom "-v_%s_k_y * y * mass"', fixID, fixID);
+strings{end+1} =     sprintf('variable fZ%s atom "-v_%s_k_z * z * mass"', fixID, fixID);
+
+%Set up energy for use in minimisation:
+strings{end+1} =	 sprintf('variable %s_E atom "v_%s_k_x * x * x / 2 + v_%s_k_y * y * y / 2 + v_%s_k_z * z * z / 2"', fixID, fixID, fixID, fixID);
+
+strings{end+1} =     sprintf('fix %s %s addforce v_%s_fX v_%s_fY v_%s_fZ energy v_%s_E', fixID, groupName, fixID, fixID, fixID, fixID);
+strings{end+1} = '';
+end
+
