db02e232dcbcb2578364e929af7031579eac5af8
Elliot Bentine <elliot.bentine@physics.ox.ac.uk>
2019-04-10 17:36:43 +0100

    Add benchmark scripts for batch running.

--- Examples/Benchmark.m
+++ Examples/Benchmark.m
@@ -1,21 +1,17 @@
-%% Simple example
-% This simple example script creates a single species ion trap. N ions are
-% confined within this trap, and are cooled by interaction with a langevin
-% bath. The positions of atoms are output every 10 timesteps, and a secular
-% velocity is also output by averaging atomic velocities over each RF
-% cycle.
 close all
 clearvars
 
-NumberOfIons = 1000;
+if ~exist('NumberOfIons', 'var')
+    NumberOfIons = 1000;
+end
 
-%Create an empty experiment.
+if ~exist('GPUAccel', 'var')
+    GPUAccel = 1;
+end
+
 sim = LAMMPSSimulation();
-sim.GPUAccel = true;
+sim.GPUAccel = GPUAccel;
 
-% Add a simulation box. This determines the region that will be simulated.
-% The simulation box may expand beyond these limits, but these specify the
-% minimum dimensions of the simulation box.
 sim.SetSimulationDomain(1e-3,1e-3,1e-3);
 
 % Add some atoms:
@@ -23,23 +19,26 @@ charge = 1;
 mass = 100;
 ions = sim.AddAtomType(charge, mass);
 createIonCloud(sim, 1e-5, ions, NumberOfIons, 1337);
-%sim.Add(custom('fix rebalance all balance 1000 1.3 shift xyz 10 1.05'))
-%Add the linear Paul trap electric field.
+
+% Add trap
 RF = 3.85e7;
 z0 = 5.5e-3; r0 = 7e-3; geomC = 0.244;
 [vs(1), vs(2)] = getVs4aq(ions, RF, z0, r0, geomC, -0.01, 0.3);
 sim.Add(linearPT( vs(1), vs(2), 5.5e-3, 7e-3, 0.244, RF));
 
-%Add some damping bath
+% Add bath
 sim.Add(langevinBath(3e-3, 1e-5));
 
 %Configure outputs.
-sim.Add(dump('positions.txt', {'id', 'x', 'y', 'z'}, 10));
-sim.Add(dump('secV.txt', {'id', timeAvg({'vx', 'vy', 'vz'}, 1/RF)}));
+sim.Add(dump('positions.txt', {'id', 'x', 'y', 'z'}, 100));
+sim.Add(dump('secV.txt', {'id', timeAvg({'vx', 'vy', 'vz'}, 100)}));
 
 % Run simulation
 sim.Add(evolve(10000));
+
+timer = tic;
 sim.Execute();
+walltime = toc(timer);
 
 figure;
 [timestep, ~, x,y,z] = readDump('positions.txt');
