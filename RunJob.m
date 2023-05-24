b3c97b3be864a5b801519b5858db44062659e47e
lewanlab <lewanlab@gmail.com>
2023-05-24 14:35:06 -0600

    Update RunJob.m

--- RunJob.m
+++ RunJob.m
@@ -18,7 +18,7 @@ cd (directory)
 % Starts simulation. In this case, we are simulating 100 Ca+ ions and 50
 % ions of mass 35amu and charge +e. To see details, open "FullSim.m"
 
-FullSim_wRawVel(filename,750,150,20,1);
+FullSim_wRawVel(filename,100,20,30,1);
 
 % Generate simulated experimental image. To use this feature, the fourth
 % input of FullSim must have been set to 1, otherwise it can be set to something else to
