52ebe4c24605d17ba675f07f695b47bd116b4e8b
lewanlab <lewanlab@gmail.com>
2023-05-30 11:21:49 -0600

    Update FullSim.m

--- Simulation Scripts/FullSim.m
+++ Simulation Scripts/FullSim.m
@@ -90,7 +90,7 @@ VTKick = langevinBath(1435.48, 30e-7,DarkGroupActive);
 sim.Add(VTKick);
 sim.Add(evolve(minimisationSteps));
 sim.Remove(VTKick);
-sim.Add(evolve(10));
+sim.Add(evolve(1000));
 
 
 %Execute 
