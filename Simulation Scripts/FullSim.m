0811512cdc8a4f49e46ab2e04fbca7b06e2f4a0a
lewanlab <lewanlab@gmail.com>
2023-06-02 16:36:54 -0600

    Update FullSim.m

--- Simulation Scripts/FullSim.m
+++ Simulation Scripts/FullSim.m
@@ -145,12 +145,19 @@ vrms2z = [zeros(NumberCa+NumberDark,1) vrms2z] ;
 Darkvrms2x = sqrt(vrms2x(NumberCa+1:end,:));
 Darkvrms2y = sqrt(vrms2y(NumberCa+1:end,:));
 Darkvrms2z = sqrt(vrms2z(NumberCa+1:end,:));
-size(Darkvrms2x)
-size(Darkvrms2y)
-size(Darkvrms2z)
-size(vrms2x)
-size(vrms2y)
-size(vrms2z)
+%Adding up each element of each array to get a total vrms for each dark Ion at every point in time
+Darkvrms2 = Darkvrms2x + Darkvrms2y + Darkvrms2z
+size(Darkvrms2)
+Darkvrms2x(1,1)
+Darkvrms2y(1,1)
+Darkvrms2z(1,1)
+Darkvrms2(1,1)
+%size(Darkvrms2x)
+%size(Darkvrms2y)
+%size(Darkvrms2z)
+%size(vrms2x)
+%size(vrms2y)
+%size(vrms2z)
 
 %Store final individual RMS velocities separately 
 vrmsfinalx = sqrt(vrms2x(:,end));
