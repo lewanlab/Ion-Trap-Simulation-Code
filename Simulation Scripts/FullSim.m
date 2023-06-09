8f4e4a34e2e8e53117ce36bda66ca7a73fac9bc5
lewanlab <lewanlab@gmail.com>
2023-06-09 10:44:25 -0600

    Update FullSim.m

--- Simulation Scripts/FullSim.m
+++ Simulation Scripts/FullSim.m
@@ -93,7 +93,7 @@ sim.Add(evolve(interval));
 % (I don't know what the time for this evolution should be)
 % Then finally execute it
 %Add Langevin Bath just to the "Active" Dark Ion Group
-VTKick = langevinBath(1800000, 30e-7,DarkGroupActive);
+VTKick = langevinBath(1850000, 30e-7,DarkGroupActive);
 sim.Add(VTKick);
 sim.Add(evolve(1));
 sim.Remove(VTKick);
