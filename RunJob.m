a5d3cc95e275c54abf66297fd3a8d8d7428f8723
lewanlab <lewanlab@gmail.com>
2023-06-14 13:10:32 -0600

    Update RunJob.m

--- RunJob.m
+++ RunJob.m
@@ -4,7 +4,7 @@ SetPaths();
 
 % Define the file name for the simulation. Make sure not to use an
 % existing one. The output folder will have the same name
-filename = '100Ca20Dark2VTKickTestNew'; %Type desired name here
+filename = '100Ca20Dark2VTKickTestNew2'; %Type desired name here
 
 % Define destination folder 
 directory = '/data/lewandow/elka1772/'; %Type your desired path here
