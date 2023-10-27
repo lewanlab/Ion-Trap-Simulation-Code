74cb08257632310f543e3a5c33667b44e516279a
lewanlab <lewanlab@gmail.com>
2023-10-27 11:34:22 -0600

    updating instructions

--- RunJob.m
+++ RunJob.m
@@ -4,26 +4,32 @@ SetPaths();
 
 % Define the file name for the simulation. Make sure not to use an
 % existing one. The output folder will have the same name
-filename = 'test'; %Type desired name here
+filename = 'helloworld'; %Type desired name here
 
 % Define destination folder 
-directory = '/data/lewandow/olkr0373/spectroscopy/'; %Type your desired path here
+directory = '/data/lewandow/olkr0373/'; %Type your desired path 
+% here. Note that the "data" branch will have more space for 
+% large data files than the "users" branch where the code will be located
+% also note, you gotta use your own user file, not olivias :) 
 
 % Create destination folder and move to that folder
 mkdir(directory, filename);
 directory = insertAfter(directory,length(directory),filename);
 cd (directory)
 
-%% Call simulation and other output functions
-% Starts simulation. In this case, we are simulating 100 Ca+ ions and 50
-% ions of mass 35amu and charge +e. Added rf voltage and endcap on 10/2023.
+% Starts simulation. In this case, we are simulating 400 Ca+ ions and 700
+% ions of mass 131amu and charge +e, RF of 400 and endcap of 3.
 %To see details, open "FullSim.m"
+% Also useful is FullSim_wRawVel, which will output raw velocity files. 
+%this has the same inputs as FullSim.
+% Also useful is FullSim_pulse, which will also take in gas identifier 
+%and the factor of heating greater than background. 
+% See simulation file for details.
 
-FullSim(filename,400,700,131,420,2.5,1);
+FullSim(filename,400,700,131,400,3,1);
 %FullSim_pulse(filename,300,100,28,400, 3,'N', 30000, 1);
 
-% Generate simulated experimental image. To use this feature, the fourth
-% input of FullSim must have been set to 1, otherwise it can be set to something else to
+% To generate a simulated experimental image, the last
+% input of any simulation script must have been set to 1, which tells the script to 
+%print out positions of ions. Otherwise it can be set to something else to
 % save time and memory
-%ExpImg(filename);%removed 10/11 due to some removal of graphing software from the cluster. 
-%Usually ran localy anyway
